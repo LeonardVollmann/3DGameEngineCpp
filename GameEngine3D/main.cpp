b30c40260c5193d722f70dc8718cb4f2feeec51a
LeonardVollmann <vollmann.leonard@gmail.com>
2014-10-11 16:30:50 +0200

    Initial Commit

---
+++ GameEngine3D/main.cpp
@@ -0,0 +1,15 @@
+//
+//  main.cpp
+//  GameEngine3D
+//
+//  Created by Leonard Vollmann on 11/10/14.
+//  Copyright (c) 2014 Leonard Vollmann. All rights reserved.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
