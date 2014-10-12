8c8d49a4828ae7401b364a806e9bb7f96297cac0
LeonardVollmann <vollmann.leonard@gmail.com>
2014-10-12 08:50:35 +0200

    Added linear interpolation and a class template for vector2

--- GameEngine3D/math.h
+++ GameEngine3D/math.h
@@ -117,9 +117,50 @@ public:
     {
         return *this / this->length();
     }
+    
+    inline Vector<T, D> lerp(const Vector<T, D> &r, const T &beta) const
+    {
+        Vector<T, D> result;
+        for (unsigned int i = 0; i < D; i++) {
+            result[i] = (1 - beta) * *this[i] + beta * r[i];
+        }
+        return result;
+    }
 protected:
 private:
     T *values[D];
+};
+
+template <typename T>
+class Vector2 : public Vector<T, 2>
+{
+public:
+    Vector2<T>() {}
+    
+    Vector2<T>(const Vector2<T> &r)
+    {
+        *this = r;
+    }
+    
+    Vector2<T>(const T &x, const T &y)
+    {
+        *this[0] = x;
+        *this[1] = y;
+    }
+    
+    inline T getX() const { return values[0]; }
+    inline T getY() const { return values[1]; }
+    
+    inline void setX(const T &x) { *this[0] = x; }
+    inline void setY(const T &y) { *this[1] = y; ]
+        
+    inline void set(const T &x, const T &y)
+    {
+        this->setX(x);
+        this->setY(y);
+    }
+protected:
+private:
 };
 
 #endif /* defined(__GameEngine3D__math__) */
